47be8862bd88024e6ffd598cddd4579788952df4
Jungdo Lee <lee.jungdo@gmail.com>
2018-10-29 19:58:29 +0900

    add eslint and apply popular style guide

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "standard",
+  "rules": {
+    "semi": ["off", "never"],
+    "quotes": ["error", "single"],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+  }
+};
