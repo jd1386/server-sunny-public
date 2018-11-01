1a084ef32d968469c85b5c9fa6007c59782ef455
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-01 17:42:27 +0900

    add play route

--- routes/play.js
+++ routes/play.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var Article = require('../models').Article;
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+require('dotenv').config();
+const router = express.Router();
+const Article = require('../models').Article;
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
@@ -8,11 +12,17 @@ const Op = Sequelize.Op;
 router.get('/', async (req, res, next) => {
   try {
     let article = await Article.findByPk(req.query.article_id);
-
-    console.log(article.get('title'));
-    res.end(article.get('title'));
-
-    // res.send('data/1.mp3');
+    const apiUrl = 'https://naveropenapi.apigw.ntruss.com/voice/v1/tts';
+    const options = {
+      url: apiUrl,
+      form: { speaker: 'mijin', speed: '-1', text: article.title },
+      headers: { 'X-NCP-APIGW-API-KEY-ID': process.env.NAVER_CLOVA_ID, 'X-NCP-APIGW-API-KEY': process.env.NAVER_CLOVA_SECRET }
+    };
+    const writeStream = fs.createWriteStream(`./tmp/article_${article.get('id')}.mp3`);
+    const _req = request.post(options).on('response', function (response) {
+      _req.pipe(writeStream); // file로 출력
+      _req.pipe(res); // 브라우저로 출력
+    });
   } catch (err) {
     next(err);
   }
