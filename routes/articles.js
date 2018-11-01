81fd5d725da9b13e98ea8c6a830337b31577c222
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-01 15:42:29 +0900

    add default attributes to display

--- routes/articles.js
+++ routes/articles.js
@@ -6,43 +6,41 @@ const Op = Sequelize.Op;
 
 /* GET articles listing. */
 router.get('/', async (req, res, next) => {
-  try {
-    let category = req.query.category_id;
+  let category = req.query.category_id;
 
-    if (category) {
-      let articles = await Article.findAll({
-        where: {
-          category_id: category,
-          rank: {
-            [Op.not]: null
-          },
-          content: {
-            [Op.not]: null
-          }
+  if (category) {
+    let articles = await Article.findAll({
+      where: {
+        category_id: category,
+        rank: {
+          [Op.not]: null
         },
-        order: [
-          ['rank', 'asc']
-        ]
-      });
-      res.json(articles);
-    } else {
-      let articles = await Article.findAll({
-        where: {
-          rank: {
-            [Op.not]: null
-          },
-          content: {
-            [Op.not]: null
-          }
+        content: {
+          [Op.not]: null
+        }
+      },
+      order: [
+        ['rank', 'asc']
+      ],
+      attributes: ['id', 'title', 'publisher', 'image_url', 'file_url', 'rank']
+    });
+    res.json(articles);
+  } else {
+    let articles = await Article.findAll({
+      where: {
+        rank: {
+          [Op.not]: null
         },
-        order: [
-          ['rank', 'asc']
-        ]
-      });
-      res.json(articles);
-    }
-  } catch (err) {
-    next(err);
+        content: {
+          [Op.not]: null
+        }
+      },
+      order: [
+        ['rank', 'asc']
+      ],
+      attributes: ['id', 'title', 'publisher', 'image_url', 'file_url', 'rank']
+    });
+    res.json(articles);
   }
 });
 
