207426975b0a9e0922556db3e2aa532332bfba70
Andrea Kim <andrea.kimmj@gmail.com>
2018-11-02 19:22:23 +0900

    naverSearch done

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const articlesRouter = require('./routes/articles');
-const categoriesRouter = require('./routes/categories');
-const sequelize = require('./models').sequelize;
+const indexRouter = require("./routes/index");
+const articlesRouter = require("./routes/articles");
+const categoriesRouter = require("./routes/categories");
+const searchRouter = require("./routes/search");
+const sequelize = require("./models").sequelize;
 
 const app = express();
+
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 sequelize.sync();
 
-app.use('/', indexRouter);
-app.use('/articles', articlesRouter);
-app.use('/categories', categoriesRouter);
+app.use("/", indexRouter);
+app.use("/search", searchRouter);
+app.use("/articles", articlesRouter);
+app.use("/categories", categoriesRouter);
 
 module.exports = app;
