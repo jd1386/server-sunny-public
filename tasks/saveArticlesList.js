50806058c3104b96816708131c136672c0d37a37
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-02 18:48:03 +0900

    fix async await

--- tasks/saveArticlesList.js
+++ tasks/saveArticlesList.js
@@ -6,7 +6,6 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 const nullifyArticles = async (combinedAids, categoryID) => {
-  // console.log('COMBINED AIDS', combinedAids);
   await Article.findAll({
     where: {
       category_id: categoryID,
@@ -44,7 +43,7 @@ const updateOrCreateArticle = async (targetArticle) => {
 };
 
 const updateArticles = async (articlesList, categoryID) => {
-  console.log('listttttt', articlesList);
+  // console.log('listttttt', articlesList);
   /*
   combined aids는 현재 인기 기사 30개 각 aid의 array로써
   현재 인기 기사와 데이터베이스에 있지만 더이상 인기없는 기사를
@@ -67,37 +66,36 @@ const updateArticles = async (articlesList, categoryID) => {
   저장된 기사가 없는 경우 해당 인기 기사를 Article 모델을 통해
   생성합니다.
   */
-  articlesList.forEach(targetArticle => {
-    updateOrCreateArticle(targetArticle);
-  });
+  for (let i = 0; i < articlesList.length; i++) {
+    await updateOrCreateArticle(articlesList[i]);
+  }
 
   // 이번 작업에서 update된 articles를
   // 다음 작업 (기사 내용 fetch)을 위해 리턴합니다.
-  let articlesToReturn = await Article.findAll({
+  let articleURLs = await Article.findAll({
     where: {
       rank: {
         [Op.not]: null
       },
-      title: {
-        [Op.not]: null
-      }
+      publisher: null,
+      category_id: categoryID
     }
   }).then(articles => {
     return _.pluck(articles, 'source_url');
   }).catch(err => { throw err; });
 
-  return articlesToReturn;
+  return articleURLs;
 };
 
 const fetchArticleContent = async (urls) => {
-  urls.forEach(url => {
-    Scraper.getArticleContent(url)
+  for (let i = 0; i < urls.length; i++) {
+    await Scraper.getArticleContent(urls[i])
       .then(articleContent => {
         Article.findOne({
           where: { aid: articleContent.aid }
         })
           .then(savedArticle => {
-            if (savedArticle && savedArticle.aid === articleContent.aid) {
+            if (savedArticle.aid === articleContent.aid) {
               savedArticle.update({
                 publisher: articleContent.publisher
               });
@@ -106,36 +104,34 @@ const fetchArticleContent = async (urls) => {
           .catch(err => { throw err; });
       })
       .catch(err => { throw err; });
-  });
+  }
 };
 
-const saveArticlesList = async (categoryName) => {
-  let categoryID;
-  await Category.findOne({ where: { name: categoryName.toLowerCase() } })
-    .then(returnedCategory => {
-      categoryID = returnedCategory.get('id');
-    })
-    .catch(err => {
-      throw err;
-    });
+const saveArticlesList = async (categoryID) => {
   // 기사 목록을 가져온다
   let articles = await Scraper.getArticleList(categoryID);
   //
   let articlesURLsToFetch = await updateArticles(articles, categoryID);
   //
-  fetchArticleContent(articlesURLsToFetch);
+  return fetchArticleContent(articlesURLsToFetch);
 };
 
 const run = async () => {
-  // saveArticlesList('politics');
-  saveArticlesList('economy');
-  // saveArticlesList('society');
+  // saveArticlesList(1); // 정치
+  // saveArticlesList(2); // 경제
+  // saveArticlesList(3); // 사회
+  // saveArticlesList(4); // 생활
+  // saveArticlesList(5); // 세계
+
+  for (let i = 1; i < 6; i++) {
+    await saveArticlesList(i);
+  }
 
   // test
   let results = await Article.findAndCountAll({
     where: {
       category_id: {
-        [Op.between]: [1, 3]
+        [Op.between]: [1, 5]
       },
       rank: {
         [Op.not]: null
