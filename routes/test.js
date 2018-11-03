a52283e406969a1699acd7228b0db2d996fae28c
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-03 12:28:13 +0900

    start basic test

--- routes/test.js
+++ routes/test.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Article = require('../models').Article;
-const scraper = require('../scrapers');
+const Scraper = require('../scrapers');
 
 /* GET articles listing. */
 router.get('/', async (req, res, next) => {
-  let Scraper = new scraper();
+  // let scraper = new Scraper();
   // res.send(scraper.newsCategories()['politics']);
-  let results = await Scraper.getArticleList(1);
+  // let results = await scraper.getArticleList(1);
   // let results = await Scraper.getArticleContent('https://m.news.naver.com/read.nhn?aid=0003673639&oid=421&sid1=100');
+
+  let results = await Article.findAll({
+    order: [
+      ['oid', 'asc']
+    ],
+    attributes: ['id', 'publisher', 'oid']
+  });
+
   res.send(results);
 });
 
