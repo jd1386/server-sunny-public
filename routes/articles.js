7bc95503fa70ed20a809f6cf38014ba79d41aa83
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-05 17:27:25 +0900

    add source_url to commonAttributes

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ const router = express.Router();
 var Article = require('../models').Article;
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const commonAttributes = ['id', 'aid', 'title', 'image_url', 'rank', 'publisher', 'createdAt'];
+const commonAttributes = ['id', 'aid', 'title', 'source_url', 'image_url', 'rank', 'publisher', 'createdAt'];
 
 /* GET articles listing. */
 router.get('/', (req, res, next) => {
