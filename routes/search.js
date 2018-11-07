db7d77a047a39f122c627e51a7142e59faae2498
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-07 13:37:33 +0900

    refactor

--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const sanitizeHtml = require('sanitize-html');
+const _ = require('underscore');
 
 require('dotenv').config();
 var clientId = process.env.NAVER_CLIENT_ID;
 var clientSecret = process.env.NAVER_CLIENT_SECRET;
 
 router.get('/news', function (req, res) {
-  console.log(11111111);
   var apiUrl =
-    'https://openapi.naver.com/v1/search/news?query=' +
+    'https://openapi.naver.com/v1/search/news?display=30&query=' +
     encodeURI(req.query.query);
   var options = {
     method: 'GET',
@@ -24,19 +25,20 @@ router.get('/news', function (req, res) {
 
   request(options, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      var naverNews = JSON.parse(body).items.filter(item => {
-        if (item.link.split('/')[2] === 'news.naver.com') {
-          return item;
+      let articles = [];
+      JSON.parse(body).items.forEach(item => {
+        if (item.link.includes('news.naver.com')) {
+          let articleObj = {};
+          // sanitize <b> tag in the title
+          articleObj.title = sanitizeHtml(item.title, {
+            allowedTags: []
+          });
+          articleObj.link = item.link;
+          articles.push(articleObj);
         }
       });
-      console.log('33333#####', naverNews);
 
-      var titleLink = naverNews.map(item => {
-        return { title: item.title, link: item.link };
-      });
-      console.log('$$$$$$$$$$$$', titleLink);
-      res.set({ 'content-type': 'application/json; charset=utf-8' }); // should set utf-8 in request as well!!
-      res.send(titleLink);
+      res.send(articles);
     } else {
       res.status(response.statusCode).end();
       console.log('error = ' + response.statusCode);
