811d574b62e1680dc41f7f7f24199788f190d82d
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-08 13:57:15 +0900

    refactor

--- routes/search.js
+++ routes/search.js
@@ -8,13 +8,10 @@ require('dotenv').config();
 var clientId = process.env.NAVER_CLIENT_ID;
 var clientSecret = process.env.NAVER_CLIENT_SECRET;
 
-router.get('/news', function (req, res) {
-  var apiUrl =
-    'https://openapi.naver.com/v1/search/news?display=30&query=' +
-    encodeURI(req.query.query);
-  var options = {
+const naverConfig = (query) => {
+  return {
     method: 'GET',
-    url: apiUrl,
+    url: 'https://openapi.naver.com/v1/search/news?display=25&query=' + encodeURI(query),
     headers: {
       'Content-Type': 'text/json; charset=utf-8',
       'cache-control': 'no-cache',
@@ -22,21 +19,27 @@ router.get('/news', function (req, res) {
       'X-Naver-Client-Secret': clientSecret
     }
   };
+};
 
-  request(options, (error, response, body) => {
+router.get('/news', function (req, res) {
+  request(naverConfig(req.query.query), (error, response, body) => {
     if (!error && response.statusCode === 200) {
       let articles = [];
-      JSON.parse(body).items.forEach(item => {
-        if (item.link.includes('news.naver.com')) {
+
+      JSON.parse(body).items
+        .filter(item => {
+          return item.link.includes('news.naver.com');
+        })
+        .forEach(item => {
           let articleObj = {};
           // sanitize <b> tag in the title
           articleObj.title = sanitizeHtml(item.title, {
             allowedTags: []
           });
-          articleObj.link = item.link;
+          articleObj.source_url = item.link;
+          articleObj.createdAt = item.pubDate;
           articles.push(articleObj);
-        }
-      });
+        });
 
       res.send(articles);
     } else {
